4a3c6dda70ce98e0bc6057762c75e98767070ca2
roaahatan123 <81920260+roaahatan123@users.noreply.github.com>
2021-05-22 07:40:09 -0700

    Add files via upload

---
+++ Stores/Stores/Connection/Class_Con.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stores.Connection
+{
+    class Class_Con
+    {
+    public static    SqlConnection Con = new SqlConnection(@"Server="+ Properties.Settings.Default.server + "; Database=" + Properties.Settings.Default.database + ";Integrated Security=true");
+
+    }
+}
